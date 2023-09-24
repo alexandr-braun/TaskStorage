54c6ca15e1779a1aea7ce36cb6a8eee38096fe94
Alexandr.Braun <alexandr.braun@zfx.com>
2023-09-24 19:19:08 +0300

    Add database registrar

---
+++ cmd/taskStorage/databaseRegistrar.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "task_storage"
+	password = "task_storage"
+	dbname   = "task_storage"
+)
+
+type DatabaseRegistrar struct {
+}
+
+func NewDatabaseRegistrar() *DatabaseRegistrar {
+	return &DatabaseRegistrar{}
+}
+
+func (dr *DatabaseRegistrar) addDatabase() {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(db)
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
