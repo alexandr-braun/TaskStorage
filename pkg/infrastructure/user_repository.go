378ebe26a31d644195c5e2582fd480ed2656f2b5
Alexandr.Braun <alexandr.braun@zfx.com>
2023-10-08 18:29:16 +0300

    Add user repository Connect & Close

--- pkg/infrastructure/user_repository.go
+++ pkg/infrastructure/user_repository.go
@@ -1,1 +1,33 @@
 package infrastructure
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"taskStorage/pkg/domain"
+)
+
+type UserRepository struct {
+	db *pgx.Conn
+}
+
+func NewUserRepository(connStr string) (*UserRepository, error) {
+	db, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to database: %v", err)
+	}
+	return &UserRepository{db: db}, nil
+}
+
+func (userRepository *UserRepository) Close() error {
+	err := userRepository.db.Close(context.Background())
+	if err != nil {
+		return fmt.Errorf("unable to connect to database: %v", err)
+	}
+	return nil
+}
+
+func (userRepository *UserRepository) GetUser(id string) domain.User {
+	//TODO implement me
+	panic("implement me")
+}
