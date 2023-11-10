5c784b29cb99803000876cafa8303dab04355c09
Alexandr.Braun <alexandr.braun@zfx.com>
2023-11-10 15:45:07 +0200

    add goose migrations

---
+++ pkg/infrastructure/migrations/20231110153338_add_users_table.go
@@ -0,0 +1,26 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigrationContext(upAddUsersTable, downAddUsersTable)
+}
+
+func upAddUsersTable(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.Exec(`CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(255),
+        middle_name VARCHAR(255),
+        last_name VARCHAR(255)
+    )`)
+	return err
+}
+
+func downAddUsersTable(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.Exec("DROP TABLE IF EXISTS users")
+	return err
+}
