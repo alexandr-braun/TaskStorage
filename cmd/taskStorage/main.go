58523ad31974b6041bbad05d2448f322e99d075a
Alexandr.Braun <alexandr.braun@zfx.com>
2023-11-11 20:00:52 +0200

    Remove redundant code from main

--- cmd/taskStorage/main.go
+++ cmd/taskStorage/main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 	"taskStorage/pkg/infrastructure"
 	"taskStorage/pkg/presentation/grpc"
 )
-
-// TODO init query handlers separately
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Hello, %s !", r.URL.Path)
-}
 
 func main() {
 	cfg, err := NewConfig()
@@ -21,12 +14,6 @@ func main() {
 
 	infrastructure.NewDatabaseRegistrar().ConnectToDatabase(cfg.Database)
 	grpc.NewGrpcRegistrar().RegisterGrpcServices()
-
-	// TODO init presentation layer
-	http.HandleFunc("/", handler)
-	go func() {
-		log.Fatal(http.ListenAndServe(":8080", nil))
-	}()
 
 	select {}
 }
