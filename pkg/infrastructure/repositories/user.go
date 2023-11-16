719bea7661946c1679532a0fb764a26fe8ced7ec
Alexandr.Braun <alexandr.braun@zfx.com>
2023-11-16 22:32:19 +0200

    Fix user repository

--- pkg/infrastructure/repositories/user.go
+++ pkg/infrastructure/repositories/user.go
@@ -1,37 +1,26 @@
 package repositories
 
 import (
-	"context"
 	"errors"
-	"fmt"
 	"github.com/jackc/pgx/v5"
 	"taskStorage/pkg/domain"
+	"taskStorage/pkg/infrastructure"
 )
 
 type PostgreSqlUserRepository struct {
-	db *pgx.Conn
+	dbConnectionFactory infrastructure.DBConnectionFactory
 }
 
-func NewPostgreSqlUserRepository(connStr string) (*PostgreSqlUserRepository, error) {
-	db, err := pgx.Connect(context.Background(), connStr)
-	if err != nil {
-		return nil, fmt.Errorf("unable to connect to database: %v", err)
-	}
-	return &PostgreSqlUserRepository{db: db}, nil
-}
-
-func (userRepository *PostgreSqlUserRepository) Close() error {
-	err := userRepository.db.Close(context.Background())
-	if err != nil {
-		return fmt.Errorf("unable to connect to database: %v", err)
-	}
-	return nil
+func NewPostgreSqlUserRepository(dbConnectionFactory infrastructure.DBConnectionFactory) (*PostgreSqlUserRepository, error) {
+	return &PostgreSqlUserRepository{dbConnectionFactory: dbConnectionFactory}, nil
 }
 
 func (userRepository *PostgreSqlUserRepository) GetUser(id string) (*domain.User, error) {
 	var user domain.User
+	var dbConnection, _ = userRepository.dbConnectionFactory.NewConnection()
+	defer dbConnection.Close()
 
-	err := userRepository.db.QueryRow(context.Background(),
+	err := dbConnection.QueryRow(
 		"SELECT id, first_name, middle_name, last_name"+
 			"FROM users"+
 			"WHERE id = $1", id).Scan(&user.Id, &user.FirstName, &user.MiddleName, &user.LastName)
