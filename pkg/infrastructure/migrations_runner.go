262d5af279f41d22e9f3eb6e105fd60427f2023b
Alexandr.Braun <alexandr.braun@zfx.com>
2023-11-21 22:56:25 +0200

    Add migrations error handling

--- pkg/infrastructure/migrations_runner.go
+++ pkg/infrastructure/migrations_runner.go
@@ -1,18 +1,27 @@
 package infrastructure
 
 import (
+	"database/sql"
 	"github.com/pressly/goose/v3"
 	"log"
 )
 
 func RunMigrations(dbConnectionFactory DBConnectionFactory) error {
-	var dbConnection, _ = dbConnectionFactory.NewConnection()
+	var dbConnection, dbConErr = dbConnectionFactory.NewConnection()
+	if dbConErr != nil {
+		log.Fatal("Error connecting to DB:", dbConErr)
+	}
+
 	if err := goose.Up(dbConnection, "pkg/infrastructure/migrations"); err != nil {
 		log.Fatal("Error applying migrations:", err)
 	}
 
-	// TODO handle the error
-	defer dbConnection.Close()
+	defer func(dbConnection *sql.DB) {
+		closeErr := dbConnection.Close()
+		if closeErr != nil {
+			log.Printf("Failed to close database connection: %v", closeErr)
+		}
+	}(dbConnection)
 
 	return nil
 }
