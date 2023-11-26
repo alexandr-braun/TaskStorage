8520d30aebd11b2975b5fa4fe0fff2d9f02a5e20
Alexandr.Braun <alexandr.braun@zfx.com>
2023-11-26 20:09:39 +0200

    Add task change status tests

--- pkg/domain/task/task_test.go
+++ pkg/domain/task/task_test.go
@@ -44,3 +44,62 @@ func TestAppendComment(t *testing.T) {
 		t.Errorf("timestamp of the comment is in the future")
 	}
 }
+
+func TestChangeStatus_ValidTransitions(t *testing.T) {
+	tests := []struct {
+		initialStatus TaskStatus
+		newStatus     TaskStatus
+		expectedError bool
+	}{
+		{New, InProgress, false},
+		{InProgress, Completed, false},
+	}
+
+	for _, tt := range tests {
+		task := Task{
+			Id:        1,
+			Title:     "Sample Task",
+			Status:    tt.initialStatus,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		}
+
+		err := task.ChangeStatus(tt.newStatus)
+
+		if (err != nil) != tt.expectedError {
+			t.Errorf("ChangeStatus(%v): expected error: %v, got: %v", tt.newStatus, tt.expectedError, err)
+		}
+
+		if err == nil && task.Status != tt.newStatus {
+			t.Errorf("ChangeStatus(%v): expected status: %v, got: %v", tt.newStatus, tt.newStatus, task.Status)
+		}
+	}
+}
+
+func TestChangeStatus_InvalidTransitions(t *testing.T) {
+	tests := []struct {
+		initialStatus TaskStatus
+		newStatus     TaskStatus
+	}{
+		{New, Completed},
+		{InProgress, New},
+		{Completed, InProgress},
+		{Completed, New},
+	}
+
+	for _, tt := range tests {
+		task := Task{
+			Id:        1,
+			Title:     "Sample Task",
+			Status:    tt.initialStatus,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		}
+
+		err := task.ChangeStatus(tt.newStatus)
+
+		if err == nil {
+			t.Errorf("ChangeStatus(%v) from %v: expected an error, got nil", tt.newStatus, tt.initialStatus)
+		}
+	}
+}
