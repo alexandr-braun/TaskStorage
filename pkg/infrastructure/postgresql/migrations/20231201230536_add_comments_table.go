d67397071e42c82dd42305290bfb06c049b55419
Alexandr.Braun <alexandr.braun@zfx.com>
2023-12-01 23:10:46 +0200

    Add tasks migrations

---
+++ pkg/infrastructure/postgresql/migrations/20231201230536_add_comments_table.go
@@ -0,0 +1,29 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigrationContext(upAddCommentsTable, downAddCommentsTable)
+}
+
+func upAddCommentsTable(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.Exec(`CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    task_id INT NOT NULL,
+    text TEXT NOT NULL,
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
+)`)
+	return err
+}
+
+func downAddCommentsTable(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.Exec("DROP TABLE IF EXISTS comments")
+	return err
+}
---
+++ pkg/infrastructure/postgresql/migrations/20231201230536_add_tasks_table.go
@@ -0,0 +1,33 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigrationContext(upAddTasksTable, downAddTasksTable)
+}
+
+func upAddTasksTable(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.Exec(`CREATE TABLE tasks (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description TEXT,
+            priority INT NOT NULL,
+            status INT NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            deadline TIMESTAMP WITH TIME ZONE,
+            assigned_to_user_id INT,
+            categories TEXT[],
+            FOREIGN KEY (assigned_to_user_id) REFERENCES users(id)
+        )`)
+	return err
+}
+
+func downAddTasksTable(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.Exec("DROP TABLE IF EXISTS tasks")
+	return err
+}
